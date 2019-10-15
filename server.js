c3fcd10add18fe7be2a148907d7bac5828fbc7bb
Jeff Miller <jeffmiller@Jeffs-MacBook-Pro.local>
2019-10-14 22:17:10 -0500

    hw update

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ var express = require("express");
 
 var path = require("path");
 
-require("./routing/apiRoutes")(app);
+var app = express();
+var PORT = 3000;
 
-require("./routing/htmlRoutes")(app);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+var APIn= require("./app/data/friends")
+
+// require("./routing/apiRoutes")(app);
+
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
